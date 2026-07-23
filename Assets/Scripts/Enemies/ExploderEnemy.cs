f2d3c1b3ae8027523f73d9772f3e914f5092059d
steph1saurus <162734991+steph1saurus@users.noreply.github.com>
2026-07-23 18:25:01 -0400

    enemyattack > attack

--- Assets/Scripts/Enemies/ExploderEnemy.cs
+++ Assets/Scripts/Enemies/ExploderEnemy.cs
@@ -4,17 +4,20 @@ namespace SpaceGame
 {
     public class ExploderEnemy : Enemy
     {
-        [SerializeField] private float explodeRadius;
+        [SerializeField] private float explodeRadius = 5f;
         [SerializeField ]private int explodeDamage;
+        [SerializeField] private float timeToExplode = 2f;
 
         //don't need to call Move() because they exist in the parent Enemy
         // you can type override
-        public override void EnemyAttack(float interval, float radius, float damage)
+        public override void Attack(float interval)
         {
-            base.EnemyAttack(interval, explodeRadius, explodeDamage);
+            base.Attack(interval);
+            //if distance to target is <= 5f
+            //then 
         }
 
-        
+
 
     }
 }
